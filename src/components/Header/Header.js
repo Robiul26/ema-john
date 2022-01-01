ac340355defdf2118d2fc376b72127436bd52a98
robiul26 <robiulrobi26@gmail.com>
2022-01-01 14:35:10 +0600

    url

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import './Header.css';
 
@@ -6,22 +7,22 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container">
-    <a className="navbar-brand" href="/ema-john-simple/shop">
+    <Link className="navbar-brand" to="/ema-john-simple/shop">
         <img src={logo} alt="" />
-    </a>
+    </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/ema-john-simple/shop">Shop</a>
+          <Link className="nav-link active" aria-current="page" to="/ema-john-simple/shop">Shop</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/ema-john-simple/review">Review</a>
+          <Link className="nav-link" to="/ema-john-simple/review">Review</Link>
         </li>  
          <li className="nav-item">
-          <a className="nav-link" href="/ema-john-simple/inventory">Inventory</a>
+          <Link className="nav-link" to="/ema-john-simple/inventory">Inventory</Link>
         </li>
       </ul>
     </div>
